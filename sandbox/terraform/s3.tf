8b3ac47be0af0546100461e53ffaeec3e28fa89d
ankithlakshman <55292458+ankithlakshman@users.noreply.github.com>
2023-10-20 12:31:36 -0500

    added customizations

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,9 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+resource "aws_ec2_transit_gateway" "AFTtransitgateway3" {
+  description = "example transit gateway deployed with global customizations3"
+}
+resource "aws_ec2_transit_gateway" "AFTtransitgateway4" {
+  description = "example transit gateway deployed with global customizations3"
+}
